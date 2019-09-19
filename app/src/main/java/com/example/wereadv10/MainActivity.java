0a59f844cae7192d2479a25b4c250982581b1bea
Amanimq <55226811+Amanimq@users.noreply.github.com>
2019-09-19 09:34:19 +0300

    Methods names

--- app/src/main/java/com/example/wereadv10/MainActivity.java
+++ app/src/main/java/com/example/wereadv10/MainActivity.java
@@ -1,15 +1,12 @@
 package com.example.wereadv10;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
-import com.google.firebase.auth.FirebaseAuth;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.util.Log;
 import android.view.View;
@@ -17,15 +14,9 @@ import android.widget.Button;
 import android.widget.TextView;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreSettings;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -54,14 +45,14 @@ public class MainActivity extends AppCompatActivity {
         catHere = findViewById(R.id.catHere);
         bookHere=findViewById(R.id.bookHere);
 
-        getCategoryData();
+        getCategories();
         getCat = findViewById(R.id.getCat);
 
          //getBookData();
 
     }
 
-    private void getCategoryData() {
+    private void getCategories() {
         dbSetUp.db.collection("categories")
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
@@ -79,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
     }
-    private void getBookData() {
+    private void getBooks() {
         dbSetUp.db.collection("books")
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
