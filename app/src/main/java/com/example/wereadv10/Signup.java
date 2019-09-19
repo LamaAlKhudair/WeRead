40a9438930fbcf21480f83c97bae61f18fa01e36
Noura <noura@Nouras-MacBook-Pro.local>
2019-09-19 08:26:07 +0300

    login & sign up UI

--- app/src/main/java/com/example/wereadv10/Signup.java
+++ app/src/main/java/com/example/wereadv10/Signup.java
@@ -4,11 +4,13 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -40,7 +42,8 @@ private String TAG = Signup.class.getSimpleName();
                 createUserAccount(email.getText().toString(),password.getText().toString());
             }
         });
-
+       // TextView logIN = (TextView) findViewById(R.id.logIN);
+        //logIN.setPaintFlags(logIN.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
 
     }//end onCreate()
     @Override
