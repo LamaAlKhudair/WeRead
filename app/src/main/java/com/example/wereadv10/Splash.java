75a32dab45dc401179a2bc254e51a94c4f015514
reyuof <reyuofali@gmail.com>
2019-09-21 00:15:23 +0300

    add timer to splash page

--- app/src/main/java/com/example/wereadv10/Splash.java
+++ app/src/main/java/com/example/wereadv10/Splash.java
@@ -7,7 +7,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
 
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Splash extends AppCompatActivity implements View.OnClickListener {
+    Timer timer;
+    boolean click = false;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,17 +23,57 @@ public class Splash extends AppCompatActivity implements View.OnClickListener {
         setContentView(R.layout.activity_splash);
         LinearLayout splashlin = findViewById(R.id.splash);
         splashlin.setOnClickListener(this);
+        mAuth = FirebaseAuth.getInstance();
+        timer = new Timer();
+
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                if (click)
+                    return;
+                if (checkUserLogin()) {
+                    //user is login
+                    Intent intent = new Intent(Splash.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Intent intent = new Intent(Splash.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        }, 2000);
+
     }
 
 
     @Override
     public void onClick(View view) {
-        Intent intent = new Intent(Splash.this, LoginActivity.class);
-        startActivity(intent);
+        switch (view.getId()) {
+            case R.id.splash:
+                if (checkUserLogin()) {
+                    //user is login
+                    Intent intent = new Intent(Splash.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+                else {
+                    Intent intent = new Intent(Splash.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+                break;
+        }
+
     }
 
-
-
+    private boolean checkUserLogin() {
+        mAuth = FirebaseAuth.getInstance();
+        if (mAuth.getCurrentUser() != null) {
+            return true;
+        } else
+            return false;
+    }
 
 
 }
