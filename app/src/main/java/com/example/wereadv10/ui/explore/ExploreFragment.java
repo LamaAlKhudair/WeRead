d0196816e135d180cc1d180c8e22986e4da4e264
LAMA <Lamamusaad13@gmail.com>
2019-09-24 00:05:11 +0300

    Home Page

--- app/src/main/java/com/example/wereadv10/ui/explore/ExploreFragment.java
+++ app/src/main/java/com/example/wereadv10/ui/explore/ExploreFragment.java
@@ -1,6 +1,7 @@
 package com.example.wereadv10.ui.explore;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -12,13 +13,18 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.example.wereadv10.Book;
+import com.example.wereadv10.Category;
 import com.example.wereadv10.ForgotPasswordActivity;
 import com.example.wereadv10.R;
 import com.example.wereadv10.ViewBooks;
 import com.example.wereadv10.dbSetUp;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
@@ -27,8 +33,7 @@ import java.util.List;
 
 
 public class ExploreFragment extends Fragment {
-    private Button getCat;
-    private TextView catHere, bookHere;
+
     private com.example.wereadv10.dbSetUp dbSetUp;
     TextView test ,more;
     private static final String TAG = "ExploreFragment";
@@ -50,8 +55,7 @@ public class ExploreFragment extends Fragment {
         });
         dbSetUp = new dbSetUp();
 
-        catHere = root.findViewById(R.id.catHere);
-        bookHere=root.findViewById(R.id.bookHere);
+
         more = root.findViewById(R.id.ShowMore);
         more.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,10 +65,10 @@ public class ExploreFragment extends Fragment {
             }
         });
         //getCategories();
-        getCat = root.findViewById(R.id.getCat);
         return root;
     }
     private  void getFiveBooks(){
+        final List<Book> FiveBooks = new ArrayList<Book>();
         CollectionReference bookRef = dbSetUp.db.collection("books");
         bookRef.limit(5).get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
@@ -72,8 +76,47 @@ public class ExploreFragment extends Fragment {
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if (task.isSuccessful()) {
                             for (QueryDocumentSnapshot document : task.getResult()) {
-                                System.out.println(document.getData().toString());
+                                final Book book = new Book();
+                                String book_title = document.get("book_title").toString();
+                                String summary = document.get("summary").toString();
+                                String author = document.get("author").toString();
+                                String bookCover = document.get("book_cover").toString();
+                                dbSetUp.storageRef.child("books_covers/"+bookCover).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                    @Override
+                                    public void onSuccess(Uri uri) {
+                                        book.setCover(uri.toString());
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception exception) {
+                                        // Handle any errors
+                                    }
+                                });
+                                book.setBook_title(book_title);
+                                book.setSummary(summary);
+                                book.setAuthor(author);
+                                DocumentReference doc = document.getDocumentReference("book_category");
+                                String path = doc.getPath();
+                                String col = path.substring(0, path.indexOf("/"));
+                                String doc3 = path.substring(path.indexOf("/")+1);
+                                dbSetUp.db.collection(col).whereEqualTo("category_name", doc3)
+                                        .get()
+                                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                                if (task.isSuccessful()) {
+                                                    for (QueryDocumentSnapshot document2 : task.getResult()) {
+                                                        Category book_cat = document2.toObject(Category.class);
+                                                        book.setBook_category(book_cat);
+                                                    }
+                                                } else {
+                                                    Log.d(TAG, "Error getting documents: ", task.getException());
+                                                }
+                                                FiveBooks.add(book);
+                                            }
+                                        });
                             }
+
                         } else {
                             Log.w(TAG, "Error getting documents.", task.getException());
                         }
@@ -81,7 +124,10 @@ public class ExploreFragment extends Fragment {
                 });
 
     }
-    private void getCategories() {
+
+
+    // No need in Home Page
+    /*private void getCategories() {
         // this function retrive all the categories in the database
         // and save them in categoies list
         dbSetUp.db.collection("categories")
@@ -90,17 +136,19 @@ public class ExploreFragment extends Fragment {
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if (task.isSuccessful()) {
-                            List<String> categoies = new ArrayList<String>();
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                categoies.add(document.getData().get("category_name").toString());
-
+                            for (QueryDocumentSnapshot document2 : task.getResult()) {
+                                Category book_cat = document2.toObject(Category.class);
                             }
                         } else {
-                            Log.w(TAG, "Error getting documents.", task.getException());
+                            Log.d(TAG, "Error getting documents: ", task.getException());
                         }
-                    }
                 });
     }
+    }*/
+
+
+   /*
+    // No need in Home Page
     private void getBooks() {
         // this function retrive all the books in the system and
         // save it in books list
@@ -110,17 +158,66 @@ public class ExploreFragment extends Fragment {
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if (task.isSuccessful()) {
-                            List<String> books = new ArrayList<String>();
+
                             for (QueryDocumentSnapshot document : task.getResult()) {
-                                books.add(document.getData().toString());
+                                final Book book = new Book();
+                                String book_title = document.get("book_title").toString();
+                                String summary = document.get("summary").toString();
+                                String author = document.get("author").toString();
+                                String bookCover = document.get("book_cover").toString();
+
+                                dbSetUp.storageRef.child("books_covers/"+bookCover).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                    @Override
+                                    public void onSuccess(Uri uri) {
+                                        // Got the download URL for 'users/me/profile.png'
+                                        book.setCover(uri.toString());
+                                        //System.out.println("Uriiiii"+uri.toString());
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception exception) {
+                                        // Handle any errors
+                                    }
+                                });
+                                book.setBook_title(book_title);
+                                book.setSummary(summary);
+                                book.setAuthor(author);
+                                DocumentReference doc = document.getDocumentReference("book_category");
+                                String path = doc.getPath();
+                                String col = path.substring(0, path.indexOf("/"));
+                                String doc3 = path.substring(path.indexOf("/")+1);
+                                dbSetUp.db.collection(col).whereEqualTo("category_name", doc3)
+                                        .get()
+                                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                                if (task.isSuccessful()) {
+                                                    for (QueryDocumentSnapshot document2 : task.getResult()) {
+                                                        Category book_cat = document2.toObject(Category.class);
+                                                        book.setBook_category(book_cat);
+                                                    }
+                                                } else {
+                                                    Log.d(TAG, "Error getting documents: ", task.getException());
+                                                }
+                                               // bookList.add(book);
+                                            }
+                                        });
                             }
+//                            System.out.println("bookList is empty fromm inside!!!"+bookList.isEmpty());
+//                            adapter.updateBooksList(bookList);
                         } else {
                             Log.w(TAG, "Error getting documents.", task.getException());
                         }
                     }
                 });
-
     }
+    */
+
+
+
+   /*
+   // No need in Home Page
+
     private void getOneBook(String bookName) {
         // this function retrive one book based on BookName == book_title
         // and print it.
@@ -142,6 +239,7 @@ public class ExploreFragment extends Fragment {
                 });
 
     }
+
     private void findCat(String documentReference){
         /*dbSetUp.db.document("categories/\("+documentReference+")")
                 .get()
@@ -154,8 +252,9 @@ public class ExploreFragment extends Fragment {
                             }
                         }
                     }
-                });*/
-    }
+                });
+    }*/
+   
     private void getClubs(){
         // this function retrive all the clubs in the system and
         // save them in clubs List
