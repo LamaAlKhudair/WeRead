3ce482f3eb787cd9cf508f494e128ae5d0d49945
LAMA <Lamamusaad13@gmail.com>
2019-09-24 00:06:47 +0300

    club class

---
+++ app/src/main/java/Club.java
@@ -0,0 +1,15 @@
+public class Club {
+private String club_name;
+
+    public String getClub_name() {
+        return club_name;
+    }
+
+    public void setClub_name(String club_name) {
+        this.club_name = club_name;
+    }
+
+    public Club(String club_name) {
+        this.club_name = club_name;
+    }
+}
