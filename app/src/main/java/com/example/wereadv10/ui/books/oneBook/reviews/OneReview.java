35d756488bc397afed072ab280ee6863a2389d0e
Amanimq <55226811+Amanimq@users.noreply.github.com>
2019-10-02 08:37:40 +0300

    OneReview.java + ReviewsTab.java

--- app/src/main/java/com/example/wereadv10/ui/books/oneBook/reviews/OneReview.java
+++ app/src/main/java/com/example/wereadv10/ui/books/oneBook/reviews/OneReview.java
@@ -1,13 +1,20 @@
 package com.example.wereadv10.ui.books.oneBook.reviews;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.SpannableStringBuilder;
+import android.text.style.ForegroundColorSpan;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.wereadv10.R;
 import com.example.wereadv10.ui.books.oneBook.bookPage;
@@ -16,38 +23,68 @@ import java.util.List;
 
 public class OneReview extends AppCompatActivity {
     private Review review;
-    private EditText username;
+    private TextView username;
     private EditText revTitle;
     private EditText body;
-    private Button ButtonAdd;
+    private Button ButtonAdd , cancel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_one_review);
+        setTitle("Add Review");
         username=findViewById(R.id.User);
         revTitle=findViewById(R.id.revTitle);
         body=findViewById(R.id.body);
         ButtonAdd=findViewById(R.id.Add2);
+        cancel= findViewById(R.id.CancelRev);
         ButtonAdd.setOnClickListener(new View.OnClickListener()
 
         {
             @Override
             public void onClick (View view){
-                Intent intent = new Intent(OneReview.this,bookPage.class);
-                intent.putExtra("username",username.getText().toString());
-                intent.putExtra("revTitle",revTitle.getText().toString());
-                intent.putExtra("body",body.getText().toString());
-                startActivity(intent);
 
+                if(!revTitle.getText().toString().equals("")&&!body.getText().toString().equals(""))//or null?
+                {
+                    Intent intent = new Intent(OneReview.this,bookPage.class);
+                    intent.putExtra("username", username.getText().toString());
+                    intent.putExtra("revTitle", revTitle.getText().toString());
+                    intent.putExtra("body", body.getText().toString());
+                    startActivity(intent);
+                    finish();
+                }
+                else
+
+                    Toast.makeText(getApplicationContext(),"You Cannot Leave This Empty!",Toast.LENGTH_SHORT).show();
             }
 
         });
 
 
-
-
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog alertDialog = new AlertDialog.Builder(OneReview.this).create();
+                alertDialog.setMessage("Discard this review?");
+                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "Yes",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish();
+                            }
+                        });
+                alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "No",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                alertDialog.show();
+            }
+        });
 
     }
 
 
+
+
 }
+
--- app/src/main/java/com/example/wereadv10/ui/books/oneBook/reviews/ReviewsTab.java
+++ app/src/main/java/com/example/wereadv10/ui/books/oneBook/reviews/ReviewsTab.java
@@ -18,19 +18,14 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.wereadv10.R;
 import com.example.wereadv10.dbSetUp;
-import com.example.wereadv10.ui.profile.profileTab.User;
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class ReviewsTab extends Fragment {
     private static final String TAG = "ReviewsTab";
@@ -38,9 +33,7 @@ public class ReviewsTab extends Fragment {
     private List<Review> RevList;
     private ReviewsAdapter reviewsAdapter;
     private String book_title;
-    private String book_id;
     private Button ButtonAdd;
-    private Button ButtonAdd2;
 
     private com.example.wereadv10.dbSetUp dbSetUp = new dbSetUp();
 
@@ -54,19 +47,20 @@ public class ReviewsTab extends Fragment {
         RevList=new ArrayList<>();
         book_title = getActivity().getIntent().getExtras().getString("TITLE");
         ButtonAdd=view.findViewById(R.id.addButton);
-        ButtonAdd2=view.findViewById(R.id.Add2);
-        getRevList();
+        reviewsAdapter = new ReviewsAdapter(this.getContext(),getRevList()) ;
 
+        rv.setItemAnimator(new DefaultItemAnimator());
+        rv.setAdapter(reviewsAdapter);
+        reviewsAdapter.notifyDataSetChanged();
         //new added review
-//        if(getActivity().getIntent()!=null) {
-//            Review review = new Review(getActivity().getIntent().getExtras().getString("username"), getActivity().getIntent().getExtras().getString("revTitle"),
-//                    getActivity().getIntent().getExtras().getString("body"));
-//            if (RevList.isEmpty()){} else {
-//                RevList.add(RevList.size() - 1, review);// لأن الاراي فاضيه ماراح نستخدم هذا حالياً
-//            RevList.add(0, review);
-//            reviewsAdapter.notifyDataSetChanged();}
-//
-//        }
+        if(getActivity().getIntent()!=null) {
+            Review review = new Review(getActivity().getIntent().getExtras().getString("username"), getActivity().getIntent().getExtras().getString("revTitle"),
+                    getActivity().getIntent().getExtras().getString("body"));
+//            RevList.add(RevList.size() - 1, review); لأن الاراي فاضيه ماراح نستخدم هذا حالياً
+            RevList.add(0, review);
+            reviewsAdapter.notifyDataSetChanged();
+
+        }
 
         ButtonAdd.setOnClickListener(new View.OnClickListener()
 
@@ -79,7 +73,6 @@ public class ReviewsTab extends Fragment {
             }
 
         });
-
         return view;
 
     }
@@ -93,108 +86,34 @@ public class ReviewsTab extends Fragment {
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if (task.isSuccessful()) {
-                            reviewsAdapter = new ReviewsAdapter(ReviewsTab.this.getContext(), RevList) ;
-                            rv.setItemAnimator(new DefaultItemAnimator());
-                            rv.setAdapter(reviewsAdapter);
                             for (QueryDocumentSnapshot document : task.getResult()) {
                                 final Review review = new Review();
-                                String doc_book_title = document.getString("book");
-                                if (doc_book_title.equalsIgnoreCase(book_title)) {
+                                DocumentReference doc = document.getDocumentReference("book");
+                                String path = doc.getPath();
+                                String col = path.substring(0, path.indexOf("/"));
+                                String doc3 = path.substring(path.indexOf("/")+1);
+                                if (doc3.equalsIgnoreCase(book_title)){
                                     String rev_title = document.get("review_title").toString();
                                     String rev_text = document.get("text").toString();
                                     review.setRevTitle(rev_title);
                                     review.setText(rev_text);
-                                    // USER
                                     DocumentReference doc_user = document.getDocumentReference("user_name");
-                                    String user_name1 = doc_user.getPath();
-                                    String doc33 = user_name1.substring(user_name1.indexOf("/") + 1);
-                                    String user_name = doc33;
+                                    String user_name = doc_user.getPath();
+                                    String doc33 = user_name.substring(user_name.indexOf("/")+1);
+                                    user_name = doc33;
                                     review.setUserName(user_name);
-                                    //getBook(doc);
                                     RevList.add(review);
-                                    reviewsAdapter.notifyDataSetChanged();
                                 }
                             }
                         } else {
                             Log.w(TAG, "Error getting documents.", task.getException());
                         }
-                        for (Review review2 : RevList){
-                            System.out.println("LAMA"+review2.getRevTitle());
-                            System.out.println("LAMA2 \t"+ review2.getText());
-                        }
                     }
-
                 });
         return RevList;
     }
-   /* private void getBook(DocumentReference doc){
-        dbSetUp.db.collection("books").document(doc.getPath()).get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task2) {
-                        if (task2.isSuccessful()) {
-                            for (QueryDocumentSnapshot book_document : task2.getResult()) {
-                                String doc3 = book_document.get("book_title").toString();
-                                if (doc3.equalsIgnoreCase(book_title)){
-                                    // Convert to User Object
-//                                    dbSetUp.db.collection("users")
-//                                            .whereEqualTo("name", user_name)
-//                                            .get()
-//                                            .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-//                                                @Override
-//                                                public void onComplete(@NonNull Task<QuerySnapshot> task2) {
-//                                                    if (task2.isSuccessful()) {
-//                                                        for (QueryDocumentSnapshot user_document : task2.getResult()) {
-//                                                            User user = user_document.toObject(User.class);
-//                                                        }
-//                                                    }
-//                                            }
-//                                                                       });
-                                    RevList.add(review);
-                                }
-                            }
-                        }
-                    }
-                });
-    }
 
-    */
-    private void addReview(){
-        final Map<String, Object> rev1 = new HashMap<>();
-        rev1.put("text", "Los Angeles");
-        rev1.put("review_title", "CA");
-       dbSetUp.db.collection("books").whereEqualTo("book_title",book_title).get()
-               .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                   @Override
-                   public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                       if (task.isSuccessful()) {
-                           for (QueryDocumentSnapshot document : task.getResult()) {
 
-                               DocumentReference doc = document.getReference();
-                               rev1.put("book", doc);
-                               rev1.put("user_name", "users/lama");
-                               dbSetUp.db.collection("reviews").document("LA")
-                                       .set(rev1)
-                                       .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                           @Override
-                                           public void onSuccess(Void aVoid) {
-                                               System.out.println("DocumentSnapshot successfully written!");
-                                           }
-                                       })
-                                       .addOnFailureListener(new OnFailureListener() {
-                                           @Override
-                                           public void onFailure(@NonNull Exception e) {
-                                               Log.w(TAG, "Error writing document", e);
-                                           }
-                                       });
-                           }
-                       } else {
-                           Log.w(TAG, "Error getting documents.", task.getException());
-                       }
-                   }
-               });
 
-          }
 
 }
-
