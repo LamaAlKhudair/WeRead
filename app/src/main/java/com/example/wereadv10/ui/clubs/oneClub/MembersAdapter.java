077f22ce590df37489378eb47a24dead82d33c50
Rima AlSadhan <RimaSadhan@gmail.com>
2019-10-10 09:57:23 +0300

    [FEAT] : Add members to club page

---
+++ app/src/main/java/com/example/wereadv10/ui/clubs/oneClub/MembersAdapter.java
@@ -0,0 +1,101 @@
+package com.example.wereadv10.ui.clubs.oneClub;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.example.wereadv10.R;
+import com.example.wereadv10.dbSetUp;
+import com.example.wereadv10.ui.otherProfile.OtherProfileActivity;
+import com.example.wereadv10.ui.profile.profileTab.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MembersAdapter extends RecyclerView.Adapter<MembersAdapter.MyViewHolder>{
+
+    private Context mContext;
+    private List<User> membersList = new ArrayList<>();
+
+
+    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        public TextView member_name;
+        public ImageView member_image;
+        private CardView card;
+
+        public MyViewHolder(View view) {
+            super(view);
+            member_name =  view.findViewById(R.id.member_name);
+            member_image = view.findViewById(R.id.Members_imageView);
+            member_image.setOnClickListener(this);
+            card = view.findViewById(R.id.member_card);
+            card.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+
+            Intent intent = new Intent(view.getContext(), OtherProfileActivity.class);
+            intent.putExtra("ID", membersList.get(getAdapterPosition()).getId());
+            intent.putExtra("NAME", membersList.get(getAdapterPosition()).getName());
+            //intent.putExtra("IMAGE", membersList.get(getAdapterPosition()).getImage());
+
+            mContext.startActivity(intent);
+        }
+
+    }//End MyViewHolder Class
+
+
+    public MembersAdapter(Context mContext, List<User> l) {
+        this.mContext = mContext;
+        this.membersList = l;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.member_card, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+
+    @Override
+    public void onBindViewHolder(final MyViewHolder holder, final int position) {
+        User club = membersList.get(position);
+        //Glide.with(mContext).load(club.getImage()).apply(RequestOptions.circleCropTransform()).into(holder.member_image);
+        holder.member_name.setText(club.getName());
+    }
+
+
+    @Override
+    public int getItemCount() {
+        if (membersList!=null)
+            return membersList.size();
+        else return 0;
+    }
+
+    public void updateList(List<User> list){
+        if (list.isEmpty()){
+            Toast.makeText(this.mContext, "No readers join the club yet.", Toast.LENGTH_SHORT).show();
+            membersList = new ArrayList<User>();
+            notifyDataSetChanged();
+        }else{
+            membersList = new ArrayList<User>();
+            membersList.addAll(list);
+            notifyDataSetChanged();
+        }
+    }
+
+}
--- app/src/main/java/com/example/wereadv10/ui/clubs/oneClub/clubPage.java
+++ app/src/main/java/com/example/wereadv10/ui/clubs/oneClub/clubPage.java
@@ -1,23 +1,44 @@
 package com.example.wereadv10.ui.clubs.oneClub;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.example.wereadv10.R;
+import com.example.wereadv10.dbSetUp;
 import com.example.wereadv10.ui.clubs.oneClub.events.clubEventTab;
 import com.example.wereadv10.ui.clubs.oneClub.votes.clubVotingTab;
+import com.example.wereadv10.ui.profile.profileTab.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.tabs.TabLayout;
 
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class clubPage extends AppCompatActivity {
+
+    private static final String TAG = "Members";
+
+    private com.example.wereadv10.dbSetUp dbSetUp = new dbSetUp();
 
     public static String clubID;
     public TextView clubName;
@@ -25,44 +46,105 @@ public class clubPage extends AppCompatActivity {
     public TextView clubOwner;
     public TextView clubDescription;
 
-    private ViewPager viewPager;
-    private clubEventTab eventFragment=new clubEventTab();;
+    // Members recycler view
+    private RecyclerView rvMembers;
+    private MembersAdapter Members_adapter;
+    private RecyclerView.LayoutManager Members_LayoutManager;
+    private List<User> Members = new ArrayList<>();
+
+    // Events and Votes
+    private ViewPager BodyViewPager;
+    private clubEventTab eventFragment = new clubEventTab();;
     private clubVotingTab votingTab = new clubVotingTab();
-    clubTabsAdapter ctabsAdapter;
+    private clubTabsAdapter clubTabsAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_club_page);
         initToolBar();
+        dbSetUp = new dbSetUp();
+
+
         clubName = findViewById(R.id.club_name);
         clubImage = findViewById(R.id.club_image);
         clubOwner = findViewById(R.id.club_owner);
         clubDescription = findViewById(R.id.club_description);
 
+        // Members recycler view
+        rvMembers = findViewById(R.id.rvMembers);
+        Members_LayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        rvMembers.setLayoutManager ( Members_LayoutManager );
+        Members_adapter = new MembersAdapter(this, Members);
+        rvMembers.setAdapter(Members_adapter);
+        getMembers();
 
-
-        viewPager = findViewById(R.id.clubEvent_viewPager);
+        // Events and Votes
+        BodyViewPager = findViewById(R.id.clubEvent_viewPager);
         TabLayout tabLayout = findViewById(R.id.clubEvent_tab_layout);
         tabLayout.addTab(tabLayout.newTab());
         tabLayout.addTab(tabLayout.newTab());
-        ctabsAdapter = new clubTabsAdapter(this.getSupportFragmentManager(), tabLayout.getTabCount());
-        ctabsAdapter.addFragment(eventFragment, "Club events");
-        ctabsAdapter.addFragment(votingTab, "Club voting");
-        viewPager.setAdapter(ctabsAdapter);
-        tabLayout.setupWithViewPager(viewPager);
+        clubTabsAdapter = new clubTabsAdapter(this.getSupportFragmentManager(), tabLayout.getTabCount());
+        clubTabsAdapter.addFragment(eventFragment, "Club Events");
+        clubTabsAdapter.addFragment(votingTab, "Club Votes");
+        BodyViewPager.setAdapter(clubTabsAdapter);
+        tabLayout.setupWithViewPager(BodyViewPager);
 
         getExtras();
         initCollapsingToolbar();
 
     }
 
+    private List<User> getMembers() {
+
+        CollectionReference MemberRef = dbSetUp.db.collection("club_members");
+        MemberRef.whereEqualTo("club_id", clubID).get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                final User member = new User();
+
+                                String member_id = document.get("member_id").toString();
+
+                                // Get members info from users collection
+                                DocumentReference userRef = dbSetUp.db.collection("users").document(member_id);
+                                userRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+
+                                        if(task.isSuccessful()){
+                                            DocumentSnapshot doc = task.getResult();
+                                            member.setName(doc.get("name").toString());
+
+                                        }
+                                    }
+                                });
+
+                                member.setId(member_id);
+
+                                Members.add(member);
+
+                                Members_adapter.notifyDataSetChanged();
+                            }
+
+                        } else Log.w(TAG, "Error getting documents.", task.getException());
+
+                    }
+                });
+        return Members;
+
+    }
+
 
     private void initCollapsingToolbar() {
-        final CollapsingToolbarLayout collapsingToolbar =
-                findViewById(R.id.collapsing_toolbar);
+        final CollapsingToolbarLayout collapsingToolbar = findViewById(R.id.collapsing_toolbar);
         collapsingToolbar.setTitle(" ");
-        AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.id.appbar);
+        AppBarLayout appBarLayout = findViewById(R.id.appbar);
         appBarLayout.setExpanded(true);
 
         // hiding & showing the title when toolbar expanded & collapsed
@@ -106,17 +188,19 @@ public class clubPage extends AppCompatActivity {
 
         }
     }
+
     @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
-    }//end onSupportNavigateUp
+    }
+
 
     private void initToolBar() {
         setTitle("Club");
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }//end initToolBar()
+    }
 
 
 }
