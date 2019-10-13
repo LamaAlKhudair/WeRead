48e85b98bbd7c85f9a507198c45cefa1edc7a0e9
Rima AlSadhan <RimaSadhan@gmail.com>
2019-10-13 09:54:20 +0300

    [FEAT] : Create Club page
    - only front-end

--- app/src/main/java/com/example/wereadv10/ui/clubs/ClubsFragment.java
+++ app/src/main/java/com/example/wereadv10/ui/clubs/ClubsFragment.java
@@ -1,16 +1,20 @@
 package com.example.wereadv10.ui.clubs;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import com.example.wereadv10.R;
 
 
-public class ClubsFragment extends Fragment {
+public class ClubsFragment extends Fragment implements View.OnClickListener{
+
+    private Button createClub;
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -18,7 +22,20 @@ public class ClubsFragment extends Fragment {
 
         View root = inflater.inflate(R.layout.fragment_clubs, container, false);
 
+        createClub = root.findViewById(R.id.createClub);
+        createClub.setOnClickListener(this);
+
         return root;
 
     }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.createClub:
+                startActivity(new Intent(getContext(), CreateClub.class));
+                break;
+        }//end switch
+    }
+
 }
---
+++ app/src/main/java/com/example/wereadv10/ui/clubs/CreateClub.java
@@ -0,0 +1,16 @@
+package com.example.wereadv10.ui.clubs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.wereadv10.R;
+
+public class CreateClub extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_club);
+    }
+}
