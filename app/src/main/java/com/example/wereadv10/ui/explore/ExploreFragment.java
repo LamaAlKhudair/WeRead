f29b882e7381f3824e9b1d1c487218f13d40c673
LAMA <Lamamusaad13@gmail.com>
2019-10-14 10:26:37 +0300

    [FEAT]: View books in Homepage based on Rating
    - Edit ExploreFragment

--- app/src/main/java/com/example/wereadv10/ui/explore/ExploreFragment.java
+++ app/src/main/java/com/example/wereadv10/ui/explore/ExploreFragment.java
@@ -26,6 +26,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
@@ -103,7 +104,7 @@ public class ExploreFragment extends Fragment {
     private List<Book> getFiveBooks(){
 
         CollectionReference bookRef = dbSetUp.db.collection("books");
-        bookRef.limit(5).get()
+        bookRef.orderBy("book_rate", Query.Direction.DESCENDING).limit(5).get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
